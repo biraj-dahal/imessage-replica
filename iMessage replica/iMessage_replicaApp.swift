8241920ce130c4bcd66e93b6cbd490719c11cfb4
biraj-dahal <dahalbiraj10@gmail.com>
2025-03-12 22:55:12 -0400

    boilerplate swiftui project

---
+++ iMessage replica/iMessage_replicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMessage_replicaApp.swift
+//  iMessage replica
+//
+//  Created by Biraj Dahal on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct iMessage_replicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
