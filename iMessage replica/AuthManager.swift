110a75f50385a88674cd28a278ea7f575d246742
biraj-dahal <dahalbiraj10@gmail.com>
2025-03-13 23:03:49 -0400

    Authentication thru firebase

--- iMessage replica/AuthManager.swift
+++ iMessage replica/AuthManager.swift
@@ -5,14 +5,73 @@
 //  Created by Biraj Dahal on 3/13/25.
 //
 
-import SwiftUI
+import Foundation
+import FirebaseAuth
 
-struct AuthManager: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+@Observable
+class AuthManager {
+   
+   var user: User?
+   
+   let isMocked: Bool = false
+   
+   var isSignedIn: Bool = false
+   
+   var userEmail: String? {
+       
+       isMocked ? "kingsley@dog.com" : user?.email
+       
+   }
+   
+   private var handle: AuthStateDidChangeListenerHandle?
+   
+   init() {
+       handle = Auth.auth().addStateDidChangeListener { [weak self] _, user in
+           self?.user = user
+           self?.isSignedIn = user != nil
+       }
+   }
+   
+   deinit {
+       if let handle = handle {
+           Auth.auth().removeStateDidChangeListener(handle)
+       }
+   }
+
+    // https://firebase.google.com/docs/auth/ios/start#sign_up_new_users
+    func signUp(email: String, password: String) {
+        Task {
+            do {
+                let authResult = try await Auth.auth().createUser(withEmail: email, password: password)
+                user = authResult.user
+            } catch {
+                print(error)
+            }
+        }
+
+    }
+
+    // https://firebase.google.com/docs/auth/ios/start#sign_in_existing_users
+    func signIn(email: String, password: String) {
+        Task {
+            do {
+                let authResult = try await Auth.auth().signIn(withEmail: email, password: password)
+                user = authResult.user
+            } catch {
+                print(error)
+            }
+        }
+    }
+
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+            user = nil 
+        } catch {
+            print(error)
+        }
     }
 }
-
 #Preview {
     AuthManager()
 }
