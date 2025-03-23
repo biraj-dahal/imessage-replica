dc555a8c7dea23ac8b64337b336669b83e311610
biraj-dahal <dahalbiraj10@gmail.com>
2025-03-23 15:50:23 -0400

    AuthManager update

--- iMessage replica/AuthManager.swift
+++ iMessage replica/AuthManager.swift
@@ -10,33 +10,32 @@ import FirebaseAuth
 
 @Observable
 class AuthManager {
-   
-   var user: User?
-   
-   let isMocked: Bool = false
-   
-   var isSignedIn: Bool = false
-   
-   var userEmail: String? {
-       
-       isMocked ? "kingsley@dog.com" : user?.email
-       
-   }
-   
-   private var handle: AuthStateDidChangeListenerHandle?
-   
-   init() {
-       handle = Auth.auth().addStateDidChangeListener { [weak self] _, user in
-           self?.user = user
-           self?.isSignedIn = user != nil
+
+    var user: User?
+
+    let isMocked: Bool = false
+    
+    var isSignedIn: Bool = false
+
+    var userEmail: String? {
+
+        isMocked ? "dbiraj@gmail.com" : user?.email
+    }
+    
+    private var handle: AuthStateDidChangeListenerHandle?
+
+    init() {
+           handle = Auth.auth().addStateDidChangeListener { [weak self] _, user in
+               self?.user = user
+               self?.isSignedIn = user != nil
+           }
        }
-   }
-   
-   deinit {
-       if let handle = handle {
-           Auth.auth().removeStateDidChangeListener(handle)
+       
+       deinit {
+           if let handle = handle {
+               Auth.auth().removeStateDidChangeListener(handle)
+           }
        }
-   }
 
     // https://firebase.google.com/docs/auth/ios/start#sign_up_new_users
     func signUp(email: String, password: String) {
@@ -66,12 +65,9 @@ class AuthManager {
     func signOut() {
         do {
             try Auth.auth().signOut()
-            user = nil 
+            user = nil
         } catch {
             print(error)
         }
     }
-}
-#Preview {
-    AuthManager()
 }
