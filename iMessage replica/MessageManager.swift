e5b3d61506c4cf9d59c171780f8ce2b32ddeb1d8
biraj-dahal <dahalbiraj10@gmail.com>
2025-03-23 15:47:36 -0400

    firestore to save conversations

--- iMessage replica/MessageManager.swift
+++ iMessage replica/MessageManager.swift
@@ -8,22 +8,56 @@
 import Foundation
 import FirebaseFirestore
 
-@Observable 
+@Observable
 class MessageManager {
 
     var messages: [Message] = []
+    
+    private let dataBase = Firestore.firestore()
 
     init(isMocked: Bool = false) {
-        if isMocked {
-            messages = Message.mockedMessages
-        } else {
-            // TODO: Fetch messages from Firestore database
+            if isMocked {
+                messages = Message.mockedMessages
+            } else {
+                getMessages()
 
+            }
         }
-    }
 
-    // TODO: Save message
+        func getMessages() {
 
-    // TODO: Get messages
+            dataBase.collectionGroup("messages").addSnapshotListener { querySnapshot, error in
+
+                guard let documents = querySnapshot?.documents else {
+                    print("Error fetching documents: \(String(describing: error))")
+                    return
+                }
+                
+                let messages = documents.compactMap { document in
+                    do {
+
+                        return try document.data(as: Message.self)
+                    } catch {
+                        print("Error decoding document into message: \(error)")
+                        return nil
+                    }
+                }
+                
+                self.messages = messages.sorted(by: { $0.timestamp < $1.timestamp })
+            }
+        }
+
+        // TODO: Get messages
+        func sendMessage(text: String, username: String) {
+            do {
+
+                let message = Message(id: UUID().uuidString, text: text, timestamp: Date(), username: username)
+
+                try dataBase.collection("messages").document().setData(from: message)
+
+            } catch {
+                print("Error sending message to Firestore: \(error)")
+            }
+        }
 
 }
